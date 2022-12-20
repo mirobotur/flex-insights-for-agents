628e3a962edb1f5d729a00dafadec0febf8c1fc1
Miro <mbotur@twilio.com>
2022-12-20 18:19:47 +0100

    initial delete and rename

---
+++ flex-template-flex-insights-for-agents/public/appConfig.example.js
@@ -0,0 +1,21 @@
+var appConfig = {
+  pluginService: {
+    enabled: true,
+    url: "/plugins",
+  },
+  ytica: false,
+  logLevel: "info",
+  showSupervisorDesktopView: true,
+  custom_data: {
+    serverless_functions_protocol: "http",
+    serverless_functions_port: "3001",
+    serverless_functions_domain_flex_insights_for_agents: "localhost",
+    features: {
+      caller_id: {
+        enabled: true
+      }
+    }
+  },
+};
+
+
---
+++ flex-template-flex-insights-for-agents/src/index.ts
@@ -0,0 +1,5 @@
+import * as FlexPlugin from '@twilio/flex-plugin';
+
+import FlexInsightsForAgents from './FlexInsightsForAgents';
+
+FlexPlugin.loadPlugin(FlexInsightsForAgents);
